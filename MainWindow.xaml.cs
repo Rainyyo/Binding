12e0cc20eec247dceb06a32626f5e92a2730cd46
Rainy <Rainy@DESKTOP-NBV217U>
2022-12-13 17:00:53 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using WpfTest.View;
+using WpfTest.ViewModel;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MyClass Mc { get; set; }=new MyClass();
+        public MainWindow()
+        {   
+            InitializeComponent();//加载MainWindow
+
+            Mc.MyText = "Hello";
+            this.DataContext= Mc;   
+        }       
+
+        public ShowWindow sw;//创建一个对象   
+
+        #region Button_Click 
+        private void ShowWindow_Click(object sender, RoutedEventArgs e)
+        {
+            sw = new ShowWindow();//实例化Show Window
+            sw.WindowStartupLocation = WindowStartupLocation.CenterScreen;//事件触发是窗体显示的位置
+
+            //sw.Show();    //.Show()使窗口变为活动窗口，但是它不会阻塞程序的执行，即其他代码会继续执行
+
+            sw.ShowDialog();   //.ShowDialog();将窗口设为模态窗口，并会阻塞程序的执行，直到用户关闭该窗口为止
+        }
+        #endregion
+
+        public MainWindowViewModel mv { get; set; } = new MainWindowViewModel();
+
+        //private void Binding_Show_Loaded(object sender, RoutedEventArgs e)
+        //{   
+        //    this.DataContext = mv;
+            
+        //}
+
+        /// <summary>
+        /// Binding_Test
+        /// </summary>
+        public class MyClass : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private string mytext;
+            public string MyText
+            {
+                get
+                { return mytext; }
+                set
+                {
+                    mytext = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MyText"));
+                }
+            }
+        }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,84 @@
+﻿using DXH.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace WpfTest.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private DispatcherTimer ShowTimer;
+        public MainWindowViewModel()
+        {
+            ShowTimer = new DispatcherTimer();//实例化
+            ShowTimer.Interval = TimeSpan.FromSeconds(1);//每隔一秒触发一次
+            ShowTimer.Tick += ShowTimer_Tick;//创建事件触发器
+            ShowTimer.Start();//启动计时器   
+        }
+
+        private void ShowTimer_Tick(object sender, EventArgs e)
+        {
+            //NowTime = System.DateTime.Now.ToString();
+            NowTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");//另一种显示的文本格式  
+        }
+
+
+        private string  nowTime;//创建一个实现来实现前端的Binding
+
+       
+
+        /// <summary>
+        /// 前端 Binding  NowTime
+        /// </summary>
+        public string NowTime
+        {
+			get { return nowTime; }
+			set
+			{ 
+				nowTime = value;
+                this.OnPropertyChanged("NowTime");//改写的方法
+
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NowTime"));//自带的方法
+            }
+		}
+
+
+        #region   
+        public event PropertyChangedEventHandler PropertyChanged;//实现接口
+        /// <summary>
+        /// 对属性值的更改通知
+        /// </summary>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            VerifyPropertyName(propertyName);
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if (propertyChanged != null)
+            {
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
+                propertyChanged(this, e);
+            }
+        }
+
+        protected virtual bool ThrowOnInvalidPropertyName { get; private set; }
+
+        public void VerifyPropertyName(string propertyName)
+        {
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                string message = "Invalid property name: " + propertyName;
+                if (ThrowOnInvalidPropertyName)
+                {
+                    throw new Exception(message);
+                }
+
+                Debug.Fail(message);
+            }
+        }
+        #endregion
+    }
+}
